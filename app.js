2f83bcbb4eb63632b7e1f750c4107caf8d89206a
abhra999 <abhraneelbag1998@gmail.com>
2024-01-01 01:30:54 +0530

    Day1 Middleware#3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const app=require('express')();
 
-app.get('/',(req,res,next)=>{
-    console.log('user');
+app.use(auth);
+
+app.get('/',auth,(req,res,next)=>{
+    res.send(`user admin ${req.admin}`);
     next();
 })
 
 app.use(log);
+
+function auth(req,res,next){
+    if(req.query.admin=='true'){
+       req.admin=true; 
+       next();
+       return;
+    }
+    res.send('No Auth');
+
+}
 
 function log(req,res,next){
     console.log('log');
