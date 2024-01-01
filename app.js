76eb5136ed6edab71223466b42aab742df3fe112
abhra999 <abhraneelbag1998@gmail.com>
2024-01-01 15:24:32 +0530

    Day1 Logging-Winston#2

--- app.js
+++ app.js
@@ -2,31 +2,36 @@ const app=require('express')();
 const winston = require('winston');
 require('dotenv').config();
 
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.File({
-            filename: 'error.log',
-            level: 'error',
-            format: winston.format.json()
-          }),        
-        ]
-  });
+const { combine, timestamp, printf, colorize, align } = winston.format;
 
-  if (process.env.NODE_ENV === 'development') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
+const transports = {
+    console: new winston.transports.Console(),
+    file:  new winston.transports.File({filename: 'error.log',level: 'error'}),
+  };
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    transports.console,
+    transports.file
+  ],
+});
 
 
 app.get('/',(req,res,next)=>{
-    if(!req.admin){
-        logger.log({
-            level: 'error',
-            message: 'admin not found'
-          });
-         
-    }
+
+    logger.info('Info message');
+    logger.error('Error message');
+    logger.warn('Warning message');
+
     res.send(`user admin ${req.admin}`);
     
     next();
