eb30bbd6db8ee9f53b785aab7e7325317d06764c
abhra999 <abhraneelbag1998@gmail.com>
2024-01-01 15:50:38 +0530

    Day1 Logging-Winston#3(CrashSiteLogger)

--- app.js
+++ app.js
@@ -2,32 +2,22 @@ const app=require('express')();
 const winston = require('winston');
 require('dotenv').config();
 
-const { combine, timestamp, printf, colorize, align } = winston.format;
-
-const transports = {
-    console: new winston.transports.Console(),
-    file:  new winston.transports.File({filename: 'error.log',level: 'error'}),
-  };
-
-  winston.loggers.add('WriteLogger', {
-    level:'error',
-    file:'error.log',
-    format: winston.format.json(),
-    transports: [transports.file],
-  });
-  winston.loggers.add('ConsoleLogger', {
-    format: winston.format.json(),
-    transports: [transports.console],
-  });
-
-  const writeLogger = winston.loggers.get('WriteLogger');
-  const consoleLogger = winston.loggers.get('ConsoleLogger');
+var logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log' })
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: 'rejections.log' })
+  ]
+});
 
 app.get('/',(req,res,next)=>{
 
-    consoleLogger.info('Info message');
-    writeLogger.error('Error message');
-
+   
+    fetch('http://examplemydomain.com')
     res.send(`user admin ${req.admin}`);
     
     next();
